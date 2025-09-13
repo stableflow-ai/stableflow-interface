d11e18ae080aee3ed28f90a748dd0274ffa117d9
Emily <emily@ref.finance>
2025-09-13 09:10:15 +0800

    add block explorer.

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -2,60 +2,70 @@ const chains = {
   near: {
     chainName: "Near",
     chainIcon: "/chains/near.png",
-    chainType: "near"
+    chainType: "near",
+    blockExplorerUrl: "https://nearblocks.io/txns"
   },
   sol: {
     chainName: "Solana",
     chainIcon: "/chains/solana.png",
-    chainType: "sol"
+    chainType: "sol",
+    blockExplorerUrl: "https://solscan.io/tx"
   },
   eth: {
     chainName: "Ethereum",
     chainIcon: "/chains/ethereum.png",
     chainType: "evm",
-    chainId: 1
+    chainId: 1,
+    blockExplorerUrl: "https://etherscan.io/tx"
   },
   arb: {
     chainName: "Arbitrum",
     chainIcon: "/chains/arbitrum.png",
     chainType: "evm",
-    chainId: 42161
+    chainId: 42161,
+    blockExplorerUrl: "https://arbiscan.io/tx"
   },
   bsc: {
     chainName: "BNB Chain",
     chainIcon: "/chains/bsc.png",
     chainType: "evm",
-    chainId: 56
+    chainId: 56,
+    blockExplorerUrl: "https://bscscan.com/tx"
   },
   avax: {
     chainName: "Avalanche",
     chainIcon: "/chains/avalanche.png",
     chainType: "evm",
-    chainId: 43114
+    chainId: 43114,
+    blockExplorerUrl: "https://snowtrace.io/tx"
   },
   base: {
     chainName: "Base",
     chainIcon: "/chains/base.png",
     chainType: "evm",
-    chainId: 8453
+    chainId: 8453,
+    blockExplorerUrl: "https://basescan.org/tx"
   },
   pol: {
     chainName: "Polygon",
     chainIcon: "/chains/polygon.png",
     chainType: "evm",
-    chainId: 137
+    chainId: 137,
+    blockExplorerUrl: "https://polygonscan.com/tx"
   },
   gnosis: {
     chainName: "Gnosis",
     chainIcon: "/chains/gnosis.png",
     chainType: "evm",
-    chainId: 100
+    chainId: 100,
+    blockExplorerUrl: "https://gnosisscan.io/tx"
   },
   op: {
     chainName: "Optimism",
     chainIcon: "/chains/optimism.png",
     chainType: "evm",
-    chainId: 10
+    chainId: 10,
+    blockExplorerUrl: "https://optimistic.etherscan.io/tx"
   }
 };
 
