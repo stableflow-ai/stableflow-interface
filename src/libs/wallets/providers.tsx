dc6e0105bedd3f904de5b0c4e4146a292195ca6b
jimmygu <jimmygujh@gmail.com>
2025-09-23 00:41:19 +0800

    fix: add okxconnect to providers

--- src/libs/wallets/providers.tsx
+++ src/libs/wallets/providers.tsx
@@ -4,18 +4,22 @@ import SolanaProvider from "./solana/provider";
 import NEARProvider from "./near/provider";
 import TronProvider from "./tron/provider";
 
+import OKXConnectProvider from "./okxconnect";
+
 export default function WalletsProvider({
   children
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <RainbowProvider>
-      <SolanaProvider>
-        <NEARProvider>
-          <TronProvider>{children}</TronProvider>
-        </NEARProvider>
-      </SolanaProvider>
-    </RainbowProvider>
+    <OKXConnectProvider>
+      <RainbowProvider>
+        <SolanaProvider>
+          <NEARProvider>
+            <TronProvider>{children}</TronProvider>
+          </NEARProvider>
+        </SolanaProvider>
+      </RainbowProvider>
+    </OKXConnectProvider>
   );
 }
