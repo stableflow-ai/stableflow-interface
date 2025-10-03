7137b843ac5354cdc382cb3057d7c503fe59efc8
jimmygu <jimmygujh@gmail.com>
2025-10-04 00:07:29 +0800

    feat: add diff bg to wallet chain item

--- src/sections/wallet/index.tsx
+++ src/sections/wallet/index.tsx
@@ -30,8 +30,8 @@ export default function Wallet() {
       }}
     >
       <Total />
-      <div className="h-[calc(100%-201px)] overflow-y-auto pb-[20px] px-[10px]">
-        <div className="pt-[10px] cursor-pointer hover:rounded-[12px] hover:bg-[#EDF0F7] duration-300 border-b border-[#EDF0EF]">
+      <div className="h-[calc(100%-201px)] overflow-y-auto pt-[8px] pb-[20px] px-[10px]">
+        <div className="pt-[10px] cursor-pointer rounded-[12px] border border-[#EDF0F7] bg-[linear-gradient(90deg,_rgba(185,215,255,0.20)_0%,_rgba(185,215,255,0.00)_50%)] hover:bg-[#EDF0F7] duration-300">
           <TypeItem type="evm" />
           {/* <Token
               token={usdcEvm}
@@ -54,17 +54,17 @@ export default function Wallet() {
             totalBalance={balancesStore.evmBalances.usdtBalance}
           />
         </div>
-        <div className="mt-[4px] pt-[6px] cursor-pointer hover:rounded-[12px] hover:bg-[#EDF0F7] duration-300 border-b border-[#EDF0EF]">
+        <div className="mt-[4px] pt-[6px] cursor-pointer rounded-[12px] border border-[#EDF0F7] bg-[linear-gradient(90deg,_rgba(248,108,255,0.20)_0%,_rgba(248,108,255,0.00)_50%)] hover:bg-[#EDF0F7] duration-300">
           <TypeItem type="sol" />
           {/* <TokenSimple token={usdcSol} /> */}
           <TokenSimple token={usdtSol} />
         </div>
-        <div className="mt-[4px] pt-[6px] cursor-pointer hover:rounded-[12px] hover:bg-[#EDF0F7] duration-300 border-b border-[#EDF0EF]">
+        <div className="mt-[4px] pt-[6px] cursor-pointer rounded-[12px] border border-[#EDF0F7] bg-[linear-gradient(90deg,_rgba(1,237,151,0.20)_0%,_rgba(1,237,151,0.00)_50%)] hover:bg-[#EDF0F7] duration-300">
           <TypeItem type="near" />
           {/* <TokenSimple token={usdcNear} /> */}
           <TokenSimple token={usdtNear} />
         </div>
-        <div className="mt-[4px] pt-[6px] cursor-pointer hover:rounded-[12px] hover:bg-[#EDF0F7] duration-300 border-b border-[#EDF0EF]">
+        <div className="mt-[4px] pt-[6px] cursor-pointer rounded-[12px] border border-[#EDF0F7] bg-[linear-gradient(90deg,_rgba(210,31,16,0.20)_0%,_rgba(210,31,16,0.00)_50%)] hover:bg-[#EDF0F7] duration-300">
           <TypeItem type="tron" />
           <TokenSimple token={usdtTron} />
         </div>
