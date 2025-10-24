5229086281b5b3a8d1daa1d98770aa8de8f6dc84
gerardhalo <gerardhalo@proton.me>
2025-10-24 15:16:16 +0800

    feat: add LayerZero and USDT contract ABIs to Usdt0 service
    - Introduced LayerZero contract ABI with functions for approval, quoting, and sending.
    - Added USDT contract ABI with an approval function.
    - Updated Usdt0Service to prepare for fee handling and quoting logic.

--- src/services/usdt0/index.ts
+++ src/services/usdt0/index.ts
@@ -1,6 +1,16 @@
 class Usdt0Service {
   public async quote(params: any) {
-    
+    const result = {
+      needApprove: true,
+      quote: void 0,
+      quoteRequest: params,
+      fees: void 0,
+    };
+
+    // get msgFee
+    const sendParams = [
+      
+    ];
   }
 }
 
