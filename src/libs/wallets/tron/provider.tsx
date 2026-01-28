2ecf3833ade9e07baa26d1b26f019deb48f159a1
gerardhalo <gerardhalo@proton.me>
2026-01-28 14:44:37 +0800

    refactor: Simplify TronProvider's setWindowWallet function
    - Removed the conditional initialization of windowTronWeb and replaced it with a direct instantiation of TronWeb, improving code clarity.
    - Ensured that the address is set correctly on the new TronWeb instance for consistent wallet interaction.
    - Streamlined transaction handling by directly using the new TronWeb instance for sending raw transactions.

--- src/libs/wallets/tron/provider.tsx
+++ src/libs/wallets/tron/provider.tsx
@@ -96,19 +96,16 @@ const Content = () => {
 
   const setWindowWallet = (address?: string) => {
     const _address = address || adapter.address;
-    let windowTronWeb = (window as any).tronWeb;
-    if (!windowTronWeb) {
-      windowTronWeb = new TronWeb({
-        fullHost: chainsRpcUrls["Tron"],
-        headers: {},
-        privateKey: "",
-      });
-      _address && windowTronWeb.setAddress(_address);
-    }
+    const _tronWeb = new TronWeb({
+      fullHost: chainsRpcUrls["Tron"],
+      headers: {},
+      privateKey: "",
+    });
+    _address && _tronWeb.setAddress(_address);
     walletRef.current = new TronWallet({
       signAndSendTransaction: async (transaction: any) => {
         const signedTx = await adapter.signTransaction(transaction);
-        const result = await windowTronWeb.trx.sendRawTransaction(signedTx);
+        const result = await _tronWeb.trx.sendRawTransaction(signedTx);
         return result.txid;
       },
       address: _address,
