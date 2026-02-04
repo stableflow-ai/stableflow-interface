ecf2a8027736a05a73b49225f3ab7efedf8632e2
jimmygu <jimmygujh@gmail.com>
2026-02-04 15:32:33 +0800

    fix: Update Tron energy condition checks in BridgeButton and useBridge hook
    - Enhanced the logic for determining Tron energy acceptance by incorporating the quoteDataService condition, ensuring accurate rendering of the "Approve" button based on the OneClick service status.

--- src/views/bridge/components/button.tsx
+++ src/views/bridge/components/button.tsx
@@ -1,4 +1,5 @@
 import Button from "@/components/button";
+import { Service } from "@/services";
 import useBridgeStore from "@/stores/use-bridge";
 import useWalletsStore from "@/stores/use-wallets";
 import { useDebounceFn } from "ahooks";
@@ -32,7 +33,7 @@ export default function BridgeButton({
     }
     const quoteData = bridgeStore.quoteDataMap.get(bridgeStore.quoteDataService);
     const isFromTron = quoteData?.quoteParam?.fromToken?.chainType === "tron";
-    const isFromTronEnergy = isFromTron && bridgeStore.acceptTronEnergy;
+    const isFromTronEnergy = isFromTron && bridgeStore.acceptTronEnergy && bridgeStore.quoteDataService === Service.OneClick;
     if (quoteData?.needApprove && !isFromTronEnergy) {
       return "Approve";
     }
