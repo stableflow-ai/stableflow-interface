d5df411cdc986c0145590bf9408738b78b48062f
jimmygu <jimmygujh@gmail.com>
2026-04-22 17:07:58 +0800

    refactor: rename TractRoot to TrackRoot and enhance mobile responsiveness in Trusted component
    - Updated the import and usage of TrackRoot in app.tsx for consistency.
    - Integrated mobile detection in the Trusted component to conditionally manage Swiper autoplay and navigation controls, improving user experience on mobile devices.

--- src/app.tsx
+++ src/app.tsx
@@ -21,7 +21,7 @@ const Developer = lazy(() => import("./views/developer"));
 const DeveloperMD = lazy(() => import("./views/developer/md"));
 
 const TronEnergyModal = lazy(() => import("./views/bridge/components/tron-energy/modal"));
-const TractRoot = lazy(() => import("./components/track-root"));
+const TrackRoot = lazy(() => import("./components/track-root"));
 
 const router = createBrowserRouter([
   {
@@ -129,7 +129,7 @@ function App() {
       </Suspense>
 
       <Suspense fallback={null}>
-        <TractRoot />
+        <TrackRoot />
       </Suspense>
     </WalletsProvider>
   );
